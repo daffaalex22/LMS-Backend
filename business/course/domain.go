417d554201981547913b466b56f6fbf1e63ef452
Boomski99 <krisnadwipayana30@gmail.com>
2021-12-23 16:11:15 +0800

    feature(course): change place for checking categories and teacher

--- business/course/domain.go
+++ business/course/domain.go
@@ -25,9 +25,14 @@ type Domain struct {
 type Usecase interface {
 	Create(ctx context.Context, domain Domain) (Domain, error)
 	Update(ctx context.Context, id string, domain Domain) (Domain, error)
+	CheckTeacher(ctx context.Context, id uint) (teacher.Domain, error)
 }
 
 type Repository interface {
 	Create(ctx context.Context, domain Domain) (Domain, error)
-	Update(ctx context.Context, id string, domain Domain) (Domain, error)
+	Update(ctx context.Context, domain Domain) (Domain, error)
+	GetCourseById(ctx context.Context, id uint) (Domain, error)
+
+	CheckTeacher(ctx context.Context, id uint) (teacher.Domain, error)
+	CheckCategories(ctx context.Context, id uint) (categories.Domain, error)
 }
